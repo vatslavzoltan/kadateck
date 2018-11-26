693601db3d59bf571f58d5532896e614ea068da0
vatslavzoltan <vatslav.zoltan@gmail.com>
2018-11-27 01:47:14 +0200

    27.11

--- app/js/common.js
+++ app/js/common.js
@@ -13,5 +13,27 @@ $(function() {
 		navText:["‹ НАЗАД",'ВПЕРЕД ›'],
 		items: 1
 	});
+	$(window).on ('load', function(){
+		$('.preloader').delay(1000).fadeOut('slow');
+	});
+
+	//Scroll
+	$('.nav_list a').mPageScroll2id({
+		scrollSpeed: 900,
+		offset: 100
+	});
+
+	// $('.top').click(function () {
+	// 	$('html, body').stop().animate({
+	// 		scrollTop: 0
+	// 	}, 'slow', 'swing');
+	// });
+	// $(window).scroll(function () {
+	// 	if ($(this).scrollTop() > $(window).height()) {
+	// 		$('.top').addClass("active");
+	// 	} else {
+	// 		$('.top').removeClass("active");
+	// 	}
+	// });
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -38,6 +38,7 @@ gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/owl/owl.carousel.min.js',
+		'app/libs/PageScroll2id.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
