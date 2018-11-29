68ad57b4ec17769f98f34382b348d41b3f6d0a64
vatslavzoltan <vatslav.zoltan@gmail.com>
2018-11-29 23:24:57 +0200

    29.11

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,5 @@
 $(function() {
-
+	$($('.top_slider .info_desc')[0]).show();
 	$('.first_screen_carousel').owlCarousel({
 		loop:true,
 		margin:10,
@@ -22,18 +22,53 @@ $(function() {
 		scrollSpeed: 900,
 		offset: 100
 	});
+	// Listen to owl events:
+	var owl = $(".info_carousel.top_carousel");
+	owl.on('changed.owl.carousel', function (e) {
+		if (e.item) {
+			var index = e.item.index - 1;
+			var count = e.item.count;
+			if (index > count) {
+				index -= count;
+			}
+			if (index <= 0) {
+				index += count;
+			}
+			var info_items = $('.top_slider .info_desc');
+			info_items.hide();
+			$(info_items[index-1]).show();
+			return index;
+			
+		}
+	});
+	//Mobile menu
+	$('.menu-btn').on('click', function (e) {
+		e.preventDefault();
+		$(this).toggleClass('menu-btn_active');
+		$('.nav_list').toggleClass('active');
+	});
+	$('.nav_list li').on('click', function (e) {
+		e.preventDefault();
+		$('.nav_list').removeClass('active');
+		$('.menu-btn').removeClass('menu-btn_active');
+	});
+	//E-mail Ajax Send
+	$(".callback").submit(function () { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function () {
+			$(th).find('.success').addClass('active').css('display', 'flex').hide().fadeIn();
+			setTimeout(function () {
+				// Done Functions
+				$(th).find('.success').removeClass('active').fadeOut();
+				th.trigger("reset");
+			}, 3000);
+		});
+		return false;
+	});
 
-	// $('.top').click(function () {
-	// 	$('html, body').stop().animate({
-	// 		scrollTop: 0
-	// 	}, 'slow', 'swing');
-	// });
-	// $(window).scroll(function () {
-	// 	if ($(this).scrollTop() > $(window).height()) {
-	// 		$('.top').addClass("active");
-	// 	} else {
-	// 		$('.top').removeClass("active");
-	// 	}
-	// });
 
 });
